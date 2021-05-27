b06b85fbe7839a225aed7b04c592a34e06ae0543
Thibault <tmasse@normandiewebschool.fr>
2021-05-27 12:23:07 +0200

    Ajoutez la map, lat et long de la NWS

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const uluru = { lat: 49.42862290611395, lng: 1.0661449027664776 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
-      center: uluru,
+      center: NWS,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: NWS,
       map: map,
     });
   }
+
